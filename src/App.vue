6a317bae781d16cb6130dd2ba0b075aae424ae80
Shinichi Matsuda <matsu1983@gmail.com>
2018-09-09 14:53:29 +0900

    NEMウォレットが使えるように

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,49 @@
 <template>
-  <div id="app">
+  <v-app>
     <header>
-      <span>Vue.js PWA</span>
+      <span>{{ title }} </span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <h1>{{ message }}</h1>
-      <router-view message="router-viewよりこんにちは"></router-view>
-      <Hello message="componentよりこんにちは"></Hello>
+      <Wallet/>
     </main>
-  </div>
+  </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import Hello from './components/Hello.vue'
-
+import Wallet from './components/Wallet.vue'
+ 
 @Component({
   name: 'app',
-  // Helloをcomponentとして定義する.
   components: {
-    Hello
+    Wallet
   }
 })
 
 export default class App extends Vue {
-  private title = 'My PWA page'
-  private message = ''
-
-  mounted () {
-    console.log('mounted App')
-    this.showMsg('Hello, Vue.js and TypeScript')
-  }
-
-  showMsg (msg: string) {
-    this.message = msg
-  }
+  private title = 'My NEM wallet'
+  mounted () {}
 }
 </script>
-
+ 
 <style>
 body {
   margin: 0;
 }
-
+ 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
-
+ 
 main {
   text-align: center;
   margin-top: 40px;
 }
-
+ 
 header {
   margin: 0;
   height: 56px;
@@ -64,7 +51,7 @@ header {
   background-color: #35495E;
   color: #ffffff;
 }
-
+ 
 header span {
   display: block;
   position: relative;
